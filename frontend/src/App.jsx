b343e2baa9758bee84ffb6ee67747287f7f453f0
Bhoomi-18 <abhoomi20@gmail.com>
2025-02-06 11:08:05 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,42 @@
 // import { BrowserRouter,Routes,Route } from "react-router-dom"; 
 // import { LoginPage } from "./Routes.jsx"
 // import { SignupPage } from "./Routes.jsx"
+
 // import './App.css';
+
 // function App(){
 //   return(
 //     <BrowserRouter>
 //       <Routes>
 //         <Route path="/Login" element={<LoginPage/>}/>
 //         <Route path="/SignUp" element={<SignupPage/>}/>
+
 //       </Routes>
 //     </BrowserRouter>
 //   )
 // }
+
 // export default App;
+
 import { BrowserRouter , Routes , Route} from "react-router-dom";
-import { LoginPage , SignupPage } from "./Routers.jsx";
-import { LoginPage , SignupPage , Home , CreateProduct } from "./Routers.jsx";
+import { LoginPage , SignupPage , Home , CreateProduct } from "./Routes.jsx";
+
 import './App.css' ;
-import Home from "./pages/Home.jsx";
 // import Home from "./Pages/Home.jsx";
 // import CreateProduct from "./Pages/createProduct.jsx";
 function App (){
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage/>} />
       <Route path="/signup" element={<SignupPage/>} />
       <Route path="/createProduct" element={<CreateProduct/>} />
       
       <Route path="/" element={<Home/>} />
 
-
+    
     </Routes>
     </BrowserRouter>
   )
 }
+export default App;
