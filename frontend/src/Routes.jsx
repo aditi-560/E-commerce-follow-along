fc0c98652e7820e8786c2c597903da973ccb2239
Bhoomi-18 <abhoomi20@gmail.com>
2025-02-07 10:12:01 +0530

    milestone 12 done

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,6 @@
-import LoginPage from "./Pages/Login";
-import SignupPage from "./Pages/Signup";
-import Home from "./Pages/Home";
+import LoginPage from "./Pages/Login.jsx";
+import SignupPage from "./Pages/Signup.jsx";
+import Home from "./Pages/Home.jsx";
 import CreateProduct from './Pages/createProduct.jsx';
-export {LoginPage,SignupPage,Home,CreateProduct};
+import MyProducts from "./Pages/myProducts.jsx";
+export {LoginPage,SignupPage,Home,CreateProduct, MyProducts};
